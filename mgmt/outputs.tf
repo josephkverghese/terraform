36782f60b909dc843fdf46e11b84e4e24786dfd8
idichan <idichan@protonmail.com>
2020-04-02 07:55:54 -0700

    fixed outputs.tf

--- mgmt/outputs.tf
+++ mgmt/outputs.tf
@@ -1,5 +1,9 @@
 output "splunk_license_s3_bucket" {
-  value = aws_s3_bucket.s3_bucket.name
+  value = aws_s3_bucket.s3_bucket_splunk_license.bucket
+}
+
+output "s3_bucket_hk" {
+  value = aws_s3_bucket.s3_bucket.bucket
 }
 
 output "aws_kms" {
