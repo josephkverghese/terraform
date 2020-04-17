ef3addfae86ff6196ac380b58edafa44f79aeaa2
idichan <idichan@protonmail.com>
2020-04-16 20:02:22 -0700

    added public ip to outputs.tf

--- mgmt/outputs.tf
+++ mgmt/outputs.tf
@@ -5,3 +5,11 @@ output "s3_bucket_splunk_license" {
 output "aws_kms" {
   value = aws_kms_key.s3key.arn
 }
+
+output "bastion_public_ip" {
+  value = aws_spot_instance_request.bastionH_WindowsUser.0.public_ip
+}
+
+output "WinUser_public_ip" {
+  value = aws_spot_instance_request.bastionH_WindowsUser.1.public_ip
+}
