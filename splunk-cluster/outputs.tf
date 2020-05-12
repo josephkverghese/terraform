76fde21fa7250d5662b060cca8851c7b681d233b
idichan <idichan@protonmail.com>
2020-05-11 17:43:48 -0700

    added deployer and shc init scripts to output

--- splunk-cluster/outputs.tf
+++ splunk-cluster/outputs.tf
@@ -14,10 +14,18 @@ output "cloudwatch_group" {
   value = aws_cloudwatch_log_group.log_group.name
 }
 
-output "splunk_ec2_instance_role"{
+output "splunk_ec2_instance_role" {
   value = aws_iam_role.splunk_ec2_role.arn
 }
 
 output "splunk_shc_alb" {
   value = aws_alb.splunk_shc_alb.0.dns_name
+}
+
+output "deployer_init" {
+  value = data.template_cloudinit_config.deployer_cloud_init.rendered
+}
+
+output "shc_init" {
+  value = data.template_cloudinit_config.shc_cloud_init.rendered
 }
