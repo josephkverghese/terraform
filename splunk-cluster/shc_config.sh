8a81d2aef6f57cdb44e5852c4e4b611f26bd6780
idichan <idichan@protonmail.com>
2020-05-17 16:19:44 -0700

    overwrite inputs.conf and server.conf

--- splunk-cluster/shc_config.sh
+++ splunk-cluster/shc_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
+sudo -u splunk echo -e "[general]\nserverName = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-sh" >/data/gmnts/splunk/etc/system/local/server.conf
+sudo -u splunk echo -e "[default]\nhost = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-sh" >/data/gmnts/splunk/etc/system/local/inputs.conf
 sudo -u splunk /data/gmnts/splunk/bin/splunk edit licenser-localslave -master_uri 'https://${license_master_hostname}:${splunkmgmt}' -auth admin:${splunkadminpass}
 sudo -u splunk /data/gmnts/splunk/bin/splunk init shcluster-config -auth admin:${splunkadminpass} -mgmt_uri https://"$(curl http://169.254.169.254/latest/meta-data/local-hostname)":${splunkmgmt} -replication_port ${splunkshcrepport} -replication_factor ${splunkshcrepfact} -conf_deploy_fetch_url https://${deployer_ip}:${splunkmgmt} -secret ${shclusterkey} -shcluster_label ${shclusterlabel}
 service splunk restart
@@ -29,7 +31,7 @@ splunkasg=${splunkshcasgname}
 ready=false
 readycount=0
 shcmem=""
-k=$(((RANDOM%100+1)))
+k=$(((RANDOM % 100 + 1)))
 echo "sleeping "$k" seconds"
 sleep $k
 echo "resuming now..."
