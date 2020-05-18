a7df1719f00bc51fb18a9ffc4b3aa30cff774194
idichan <idichan@protonmail.com>
2020-05-18 13:43:33 -0700

    ixrc

--- splunk-cluster/ixrcmaster_config.sh
+++ splunk-cluster/ixrcmaster_config.sh
@@ -3,11 +3,11 @@ exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&
 sudo -u splunk echo -e "[general]\nserverName = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-ixrmaster" >/data/gmnts/splunk/etc/system/local/server.conf
 sudo -u splunk echo -e "[default]\nhost = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-ixrmaster" >/data/gmnts/splunk/etc/system/local/inputs.conf
 sudo -u splunk echo -e "[clustering]\nmode=master\nreplication_factor = ${ixrcrepf}\nsearch_factor = ${ixrcsf}\npass4SymmKey = ${ixrckey}\ncluster_label = ${ixrclabel}" >>/data/gmnts/splunk/etc/system/local/server.conf
-sudo -u splunk /data/gmnts/splunk/bin/splunk edit licenser-localslave -master_uri 'https://${license_master_hostname}:${splunkmgmt}' -auth admin:${splunkadminpass}
+sudo -u splunk /data/gmnts/splunk/bin/splunk edit licenser-localslave -master_uri 'https://${license_master_hostname}:${splunk_mgmt_port}' -auth admin:${splunkadminpass}
 service splunk restart
 sudo -u splunk /data/gmnts/splunk/bin/splunk clone-prep-clear-config -auth admin:${splunkadminpass}
 service splunk restart
-sudo -u splunk /data/gmnts/splunk/bin/splunk edit cluster-config -mode slave -master_uri 'https://${ixrcmaster}:${splunkmgmt}' -replication_port ${splunkixrcrepport} -auth admin:${splunkadminpass}
+sudo -u splunk /data/gmnts/splunk/bin/splunk edit cluster-config -mode slave -master_uri 'https://${ixrcmaster}:${splunk_mgmt_port}' -replication_port ${splunkixrcrepport} -auth admin:${splunkadminpass}
 service splunk restart
 sudo -u splunk /data/gmnts/splunk/bin/splunk edit cluster-config -mode master -replication_factor ${ixrcrepf} -search_factor ${ixrcsf} -auth admin:${splunkadminpass}
 service splunk restart
