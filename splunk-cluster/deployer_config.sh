99b34d4f058638dabdf3e94bc3001b1a099d0b5f
idichan <idichan@protonmail.com>
2020-05-24 17:59:00 -0700

    deployer set host in inputs conf

--- splunk-cluster/deployer_config.sh
+++ splunk-cluster/deployer_config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-sudo -u splunk /data/gmnts/splunk/bin/splunk edit licenser-localslave -master_uri 'https://${license_master_hostname}:${splunk_mgmt_port}' -auth admin:${splunkadminpass}
 sudo -u splunk echo -e "[general]\nserverName = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-deployer" > /data/gmnts/splunk/etc/system/local/server.conf
+sudo -u splunk echo -e "[default]\nhost = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-sh" >/data/gmnts/splunk/etc/system/local/inputs.conf
+sudo -u splunk /data/gmnts/splunk/bin/splunk edit licenser-localslave -master_uri 'https://${license_master_hostname}:${splunk_mgmt_port}' -auth admin:${splunkadminpass}
 sudo -u splunk echo -e  "[shclustering]\npass4SymmKey = ${shclusterkey}\nshcluster_label = ${shclusterlabel}" >> /data/gmnts/splunk/etc/system/local/server.conf
 service splunk restart
 #add outputs.conf to forward SH logs data to indexer cluster
