5752d57e44391734fa918e2fd831be1c9a4735aa
idichan <idichan@protonmail.com>
2020-05-25 18:46:52 -0700

    redirect to file

--- splunk-cluster/shc_config.sh
+++ splunk-cluster/shc_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
-sudo -u splunk echo -e "[general]\nserverName = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-sh" >/data/gmnts/splunk/etc/system/local/server.conf
-sudo -u splunk echo -e "[default]\nhost = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-sh" >/data/gmnts/splunk/etc/system/local/inputs.conf
+sudo -u splunk echo -e "[general]\nserverName = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-sh" > /data/gmnts/splunk/etc/system/local/server.conf
+sudo -u splunk echo -e "[default]\nhost = "$(curl http://169.254.169.254/latest/meta-data/local-hostname)"-sh" > /data/gmnts/splunk/etc/system/local/inputs.conf
 sudo -u splunk /data/gmnts/splunk/bin/splunk edit licenser-localslave -master_uri 'https://${license_master_hostname}:${splunkmgmt}' -auth admin:${splunkadminpass}
 service splunk restart
 sudo -u splunk /data/gmnts/splunk/bin/splunk init shcluster-config -auth admin:${splunkadminpass} -mgmt_uri https://"$(curl http://169.254.169.254/latest/meta-data/local-hostname)":${splunkmgmt} -replication_port ${splunkshcrepport} -replication_factor ${splunkshcrepfact} -conf_deploy_fetch_url https://${deployer_ip}:${splunkmgmt} -secret ${shclusterkey} -shcluster_label ${shclusterlabel}
